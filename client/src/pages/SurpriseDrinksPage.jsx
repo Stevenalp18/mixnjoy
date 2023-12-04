38010939031fedaee73d64addc82dec59a907fab
Steven Perez <stevenalp18@gmail.com>
2023-12-03 20:28:18 -0500

    added functionality to button and button requests new item everytime

--- client/src/pages/SurpriseDrinksPage.jsx
+++ client/src/pages/SurpriseDrinksPage.jsx
@@ -2,28 +2,61 @@ import { useEffect, useState } from "react";
 
 const SurpriseDrinksPage = () => {
   const [drink, setDrink] = useState(null);
-  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    const getRandomDrink = async () => {
-      try {
-        const response = await fetch(
-          "https://www.thecocktaildb.com/api/json/v1/1/random.php/"
-        );
-        const data = await response.json();
-        setDrink(data);
-        console.log(drink);
-      } catch (err) {
-        setError(err);
-        console.log(error);
+    const fetchRandomPresetData = async () => {
+      setLoading(true)
+      try{
+        const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+        if(!response.ok){
+          throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+        const data  = await response.json()
+    
+        if(response.ok && data != null){
+          setDrink(data)
+        }
+    
+        return data
       }
-    };
+      catch(error){
+        console.log('Erorr during fetch:', error.message)
+        throw error
+      }finally{
+        setLoading(false)
+      }
+    }
 
-    getRandomDrink();
-  }, []);
+    fetchRandomPresetData()
+  }, [])
+
+
+  const handleClick = async () => {
+    setLoading(true)
+      try{
+        const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+        if(!response.ok){
+          throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+        const data  = await response.json()
+    
+        if(response.ok && data != null){
+          setDrink(data)
+        }
+    
+        return data
+      }
+      catch(error){
+        console.log('Erorr during fetch:', error.message)
+        throw error
+      }finally{
+        setLoading(false)
+      }
+  }
 
   const renderDrinkCard = () => {
-    if (drink !== null) {
+    if (drink != null) {
       const { drinks } = drink;
       const {
         idDrink,
@@ -66,15 +99,12 @@ const SurpriseDrinksPage = () => {
     }
   };
 
-  useEffect(() => {
-    renderDrinkCard();
-  }, [drink]);
-
   return (
     <section>
       <div className="flex flex-col justify-center py-4">
-        {drink && renderDrinkCard()}
-        <button className="p-2 bg-rose-600 hover:bg-teal-400 active:bg-rose-400 text-white rounded-2xl text-center w-52 mx-auto mb-8">
+        {loading ? <div className="text-xl text-center py-12">Mixing drinks for you...</div> : drink && renderDrinkCard()}
+        <button onClick={handleClick}
+        className="p-2 bg-rose-600 hover:bg-teal-400 active:bg-rose-400 text-white rounded-2xl text-center w-52 mx-auto mb-8">
           Get Surprise Drink
         </button>
       </div>
