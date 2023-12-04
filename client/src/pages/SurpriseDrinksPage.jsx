158efd92d6cdaf1d92360f7706e6e164c8bf414c
Steven Perez <stevenalp18@gmail.com>
2023-12-03 21:28:41 -0500

    added the measures and ingredients and added some conditional rendering

--- client/src/pages/SurpriseDrinksPage.jsx
+++ client/src/pages/SurpriseDrinksPage.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 
 const SurpriseDrinksPage = () => {
   const [drink, setDrink] = useState(null);
+  const [filteredIngredients, setFilteredIngredients] = useState(null)
+  const [ingredientMeasure, setIngredientMeasure] = useState(null)
   const [loading, setLoading] = useState(true)
-
+  
   useEffect(() => {
     const fetchRandomPresetData = async () => {
       setLoading(true)
@@ -55,9 +57,49 @@ const SurpriseDrinksPage = () => {
       }
   }
 
-  const renderDrinkCard = () => {
+  const getFilteredIngredients = () => {
+    if(drink != null){
+    const arr = []
+    const drinkObj = drink.drinks[0]
+    for(let key in drinkObj){
+      for(let i = 1; i<16; i++){
+        if(key === `strIngredient${i}` && drinkObj[key] !== null){
+          arr.push(drinkObj[key])
+        }
+      }
+    }
+    console.log(arr)
+    setFilteredIngredients(arr)
+    return arr
+   }
+  }
+
+  const getIngredientsMeasure = () => {
+    if(drink != null){
+    const arr = []
+    const drinkObj = drink.drinks[0]
+    for(let key in drinkObj){
+      for(let i = 1; i<16; i++){
+        if(key === `strMeasure${i}` && drinkObj[key] !== null){
+          arr.push(drinkObj[key])
+        }
+      }
+    }
+    console.log(arr)
+    setIngredientMeasure(arr)
+    return arr
+   }
+  }
+  
+    useEffect(() => {
+      getFilteredIngredients()
+      getIngredientsMeasure()
+    }, [drink])
+
+    const renderDrinkCard = () => {
     if (drink != null) {
       const { drinks } = drink;
+      
       const {
         idDrink,
         strDrink,
@@ -66,13 +108,14 @@ const SurpriseDrinksPage = () => {
         strInstructions,
         strGlass,
       } = drinks[0];
+      
       return (
-        <div key={idDrink} className="px-8 mx-auto text-center mb-8">
+        <div key={idDrink} className="px-8 mx-auto text-center mb-2 md:mb-8">
           <div className="text-center py-4 text-xl logo-text">{strDrink}</div>
           <div className="flex flex-col md:flex-row justify-evenly ">
             <img
               src={strDrinkThumb}
-              alt=""
+              alt={`Image of the ${strDrink} ${strAlcoholic} drink`}
               width={300}
               height={400}
               className="mx-auto"
@@ -86,7 +129,13 @@ const SurpriseDrinksPage = () => {
                 Instructions: {strInstructions}
               </div>
               <div className="text-center pt-4">
-                {/* Ingredients: {renderIngredients()} */}
+                Ingredients:
+                <ol>
+                {filteredIngredients && filteredIngredients.map((ingredient, index) => {
+                  const measure = ingredientMeasure
+                  return <li key={index}>{index}: {ingredient} - {measure[index]}</li>
+                })}
+                </ol>
               </div>
               <div className="text-center pt-4"></div>
             </div>
@@ -94,15 +143,14 @@ const SurpriseDrinksPage = () => {
         </div>
       );
     } else {
-      console.log("no drink data found");
-      return;
+      return <div>no drink data found</div>
     }
   };
-
+  
   return (
     <section>
       <div className="flex flex-col justify-center py-4">
-        {loading ? <div className="text-xl text-center py-12">Mixing drinks for you...</div> : drink && renderDrinkCard()}
+        {loading ? <div className="text-xl text-center lg:py-12">Mixing drinks for you...</div> : drink && renderDrinkCard()}
         <button onClick={handleClick}
         className="p-2 bg-rose-600 hover:bg-teal-400 active:bg-rose-400 text-white rounded-2xl text-center w-52 mx-auto mb-8">
           Get Surprise Drink
